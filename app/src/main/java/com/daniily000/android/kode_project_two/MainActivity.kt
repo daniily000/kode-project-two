3eff00573607663ed3547435a52c3c1e39c65bf6
daniily000 <daniily000@yandex.ru>
2018-12-06 20:15:05 +0200

    Initial commit

---
+++ app/src/main/java/com/daniily000/android/kode_project_two/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.daniily000.android.kode_project_two
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
