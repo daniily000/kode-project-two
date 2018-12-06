ff8bfca3878da282e07dd06abd1e092057ebe33c
daniily000 <daniily000@yandex.ru>
2018-12-06 20:17:21 +0200

    Package name fix

--- app/src/main/java/com/daniily000/android/kodeprojecttwo/MainActivity.kt
+++ app/src/main/java/com/daniily000/android/kodeprojecttwo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.daniily000.android.kode_project_two
+package com.daniily000.android.kodeprojecttwo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
